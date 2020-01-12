00c72ef89e2907f27e68c0d2a0b69868e88deb22
Paul Aly <paul.aly@outlook.com>
2020-01-12 10:18:07 -0500

    Initial commit

---
+++ src/main/java/com/lumberjackdev/template/TemplateApp.java
@@ -0,0 +1,13 @@
+package com.lumberjackdev.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/lumberjackdev/template/TemplateAppTests.java
@@ -0,0 +1,13 @@
+package com.lumberjackdev.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
